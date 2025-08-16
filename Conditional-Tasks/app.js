344621c2649badde562a009eb6dd5ff2a9653dc6
NaveenKalyanam09 <naveenkalyanam009@gmail.com>
2025-08-16 13:22:37 +0530

    first question

---
+++ Conditional-Tasks/app.js
@@ -0,0 +1,24 @@
+//First Question
+
+let price = 1000;
+let finalPrice = price;
+let age = 22;
+let day = "monday";
+let isFestival = "no";
+let isStudent = "yes";
+let hasId = "yes";
+let month = "july";
+
+if(age>60){
+    if(day !== "sunday" && isFestival === "no"){
+       console.log(finalPrice = price - (price * 0.3));
+    }
+} else if(age<25){
+    if(isStudent === "yes" && hasId === "yes" && month !== "december"){
+        console.log(finalPrice = price - (price * 0.2));
+    }
+} else if (day === "sunday"){
+    console.log(finalPrice = price - (price * 0.1));
+} else {
+    console.log("Final Ticket Price: ₹" + finalPrice);
+}
