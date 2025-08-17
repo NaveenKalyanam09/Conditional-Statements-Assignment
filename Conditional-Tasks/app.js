23af28f7d410db4edfeb21d91dada5abf5f88d5e
NaveenKalyanam09 <naveenkalyanam009@gmail.com>
2025-08-17 15:23:45 +0530

    11th Question

--- Conditional-Tasks/app.js
+++ Conditional-Tasks/app.js
@@ -567,6 +567,28 @@ switch(bookingCategories){
 }
 
 
+//11th QUESTION
+
+let percentage = 86;
+let isFinalYear = true;
+
+if (percentage >= 90) {
+    console.log("Grade A");
+} else if (percentage >= 80 && percentage <= 89) {
+    console.log("Grade B");
+} else if (percentage >= 70 && percentage <= 79) {
+    console.log("Grade C");
+} else if (percentage >= 60 && percentage <= 69) {
+    console.log("Grade D");
+} else if (percentage >= 50 && percentage <= 59) {
+    console.log("Grade E");
+} else if (percentage >= 45 && percentage <= 49 && isFinalYear) {
+    console.log("Eligible For Re-Evaluation");
+} else if (percentage < 50) {
+    console.log("Fail");
+} else {
+    console.log("Enter Valid Percentage");
+}
 
   
 
